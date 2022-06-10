848d3d16cbecac7b7f37eedbaeba7bf789e6d9bd
omnipede <omnipede@naver.com>
2022-06-10 17:41:50 +0900

    테스트: 테스트용 커밋

--- youtubetl/__init__.py
+++ youtubetl/__init__.py
@@ -1,10 +1,10 @@
-from youtubetl.impl.vgg.VggYoutubeClipDownloader import VggYoutubeClipDownloader
+from youtubetl.impl.vgg.VggYoutubeClipDownloader import *
 
 
 if __name__ == '__main__':
-
-    VggYoutubeClipDownloader(
-        source_file_path='/Users/seohyeongyu/Desktop/work/naver-ai/aaclip/youtube-downloader/dataset/vggsound.csv',
-        output_path='/Users/seohyeongyu/Desktop/data/vgg',
-        pool_size=30
-    ).execute()
+    pass
+    # VggYoutubeClipDownloader(
+    #     source_file_path='/Users/seohyeongyu/Desktop/work/naver-ai/aaclip/youtube-downloader/dataset/vggsound.csv',
+    #     output_path='/Users/seohyeongyu/Desktop/data/vgg',
+    #     pool_size=30
+    # ).execute()
