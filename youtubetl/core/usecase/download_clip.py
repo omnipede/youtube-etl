4555b4e10eab1b697322d5b3130a276edbcaf0ce
omnipede <omnipede@naver.com>
2022-06-13 12:38:06 +0900

    기능: split download 기능 추가
    범위를 나누어서 다운로드 받을 수 있는 기능 추가

--- youtubetl/core/usecase/download_clip.py
+++ youtubetl/core/usecase/download_clip.py
@@ -2,17 +2,20 @@ from multiprocessing import Pool
 
 from youtubetl.core.domain.YoutubeClip import YoutubeClip
 from tqdm import tqdm
+import numpy as np
 
 
 class YoutubeClipDownloader:
 
     def __init__(self, pool_size: int = 1):
         self.pool_size = pool_size
+        self.clips = None
 
     def execute(self):
 
         # Fetch all clips to be downloaded
-        clips: [YoutubeClip] = self._fetch_clips_to_download()
+        clips: [YoutubeClip] = self.clips if self.clips is not None \
+            else self._fetch_clips_to_download()
 
         pbar = tqdm(total=len(clips))
 
@@ -39,6 +42,14 @@ class YoutubeClipDownloader:
             clip.download()
         except:
             pass
+
+    def split(self, num_split: int = 2, target: int = 0):
+        if target > num_split - 1:
+            raise Exception(f"Invalid argument: target {target} is larger than num_split {num_split}")
+
+        clips: [YoutubeClip] = self._fetch_clips_to_download()
+        split_clips = np.array_split(clips, num_split)
+        self.clips = split_clips[target]
 
     def _fetch_clips_to_download(self) -> [YoutubeClip]:
         raise Exception("Not implemented yet.")
